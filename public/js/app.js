7f2acb9fa46d8cac2f8dd8ad87341984aece656b
Pavan Kumar <pavanpolepaka1234@gmail.com>
2021-06-09 21:46:51 +0530

    Set Up app for heruko

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,5 @@
 console.log('Client side javascript file is loaded! Hello World')
 
-fetch('http://localhost:5000/weather?address=!').then((response)=>{
-    response.json().then((data) => {
-        if(data.error)
-            console.log(data.error)
-        else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
-
 const w=document.querySelector('form')
 const p1=document.querySelector('.success')
 const p2=document.querySelector('.error')
@@ -19,7 +8,7 @@ w.addEventListener('submit',(e)=>{
     console.log(loc)
     p1.textContent='Loading...'
     p2.textContent=''
-    fetch('http://localhost:5000/weather?address='+loc).then((response)=>{
+    fetch('/weather?address='+loc).then((response)=>{
     response.json().then((data) => {
         if(data.error)
             p1.textContent=data.error
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { title } = require('process')
 const forecast=require('./utils/forecast')
 
 const app = express()
+const port=process.env.PORT || 3000 
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -98,6 +99,6 @@ app.get('*' ,(req,res) => {
 })
 
 
-app.listen(5000, () => {
-    console.log('Server is up on port 5000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
